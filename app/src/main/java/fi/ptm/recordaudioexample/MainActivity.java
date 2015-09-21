00d70bdbbe810af2d40f470eed1ce5804910c574
Pasi Manninen <pasi@ptm.fi>
2015-09-21 14:23:55 +0300

    Added MediaStore intent to capture audio.

--- app/src/main/java/fi/ptm/recordaudioexample/MainActivity.java
+++ app/src/main/java/fi/ptm/recordaudioexample/MainActivity.java
@@ -1,8 +1,10 @@
 package fi.ptm.recordaudioexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -22,6 +24,9 @@ public class MainActivity extends Activity {
     private MediaRecorder mediaRecorder;
     private MediaPlayer mediaPlayer;
     private String outputFile;
+
+    // used record audio with intent
+    private final int RECORD_AUDIO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,5 +90,24 @@ public class MainActivity extends Activity {
             mediaRecorder = null;
         }
     }
+
+    // record audio with intent
+    public void recordAudioWithIntent(View view) {
+        // create Intent to record audio
+        Intent intent = new Intent("android.provider.MediaStore.RECORD_SOUND");
+        startActivityForResult(intent,RECORD_AUDIO);
+    }
+
+    // get audio from audio intent
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == RECORD_AUDIO) {
+            Uri audioUri = data.getData();
+            if (mediaRecorder != null) mediaRecorder.release();
+            mediaPlayer = MediaPlayer.create(getBaseContext(),audioUri);
+            mediaPlayer.start();
+        }
+    }
+
 
 }
